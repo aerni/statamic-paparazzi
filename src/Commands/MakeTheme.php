3c23ec820ea32a0db790ed20561ad56a684f6ec8
Michael Aerni <hello@michaelaerni.ch>
2023-03-16 14:04:29 -0400

    Make theme command work

--- src/Commands/MakeTheme.php
+++ src/Commands/MakeTheme.php
@@ -2,6 +2,7 @@
 
 namespace Aerni\Paparazzi\Commands;
 
+use Aerni\Paparazzi\Facades\Model;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
 use Statamic\Console\RunsInPlease;
@@ -10,39 +11,27 @@ class MakeTheme extends Command
 {
     use RunsInPlease;
 
-    protected $signature = 'seo:theme {name?}';
-    protected $description = 'Create a new social images theme';
+    protected $signature = 'paparazzi:theme {model?} {name?}';
+    protected $description = 'Create a new Paparazzi model theme';
 
     public function handle(): void
     {
         $this->publishTheme();
-        $this->publishLayout();
-    }
-
-    protected function publishLayout(): void
-    {
-        $source = __DIR__.'/../../resources/stubs/social_images/';
-        $target = resource_path('views/social_images/');
-        $layout = 'layout.antlers.html';
-
-        if (! File::exists($target.$layout)) {
-            File::ensureDirectoryExists($target);
-            File::copy($source.$layout, $target.$layout);
-            $this->line("<info>[✓]</info> The layout was successfully created: <comment>{$this->getRelativePath($target.$layout)}</comment>");
-        }
     }
 
     protected function publishTheme(): void
     {
+        $model = $this->argument('model') ?? $this->choice('For which model do you want to create a theme', Model::all()->map->id()->all());
         $theme = $this->argument('name') ?? $this->ask('What do you want to call the theme?', 'default');
 
-        $source = __DIR__.'/../../resources/stubs/social_images/templates';
-        $target = resource_path('views/social_images/'.$theme);
+        $source = __DIR__.'/../../resources/stubs/template.antlers.html';
+        $directory = config('paparazzi.views') . "/{$model}";
+        $file = "$directory/{$theme}.antlers.html";
 
-        if (! File::exists($target) || $this->confirm("A theme with the name <comment>$theme</comment> already exists. Do you want to overwrite it?")) {
-            File::ensureDirectoryExists($target);
-            File::copyDirectory($source, $target);
-            $this->line("<info>[✓]</info> The theme was successfully created: <comment>{$this->getRelativePath($target)}</comment>");
+        if (! File::exists($file) || $this->confirm("A theme with the name <comment>$theme</comment> already exists. Do you want to overwrite it?")) {
+            File::ensureDirectoryExists($directory);
+            File::copy($source, $file);
+            $this->line("<info>[✓]</info> The theme was successfully created: <comment>{$this->getRelativePath($file)}</comment>");
         }
     }
 
